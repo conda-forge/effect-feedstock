293947c627cfbdaff0f7751cde364a847a8c234c
Filipe Fernandes <ocefpaf@gmail.com>
2019-11-08 12:25:21 -0300

    update recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
 IF "%PY_VER:~0,1%"=="2" del effect\_test_do_py3.py
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
+
+%PYTHON% -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
